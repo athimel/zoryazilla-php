2482fd0e07934008a0284ebbbec00d3353bf99c6
Arnaud Thimel <a.thimel@gmail.com>
2013-05-23 22:48:23 +0200

    Pas de message d'erreur sur la page de mouches

--- scripts_MZ/mouches_FF.js
+++ scripts_MZ/mouches_FF.js
@@ -122,61 +122,120 @@ traiteMouches();
 displayScriptTime();
 }
 catch(e) {alert(e)}
-//============================ ZZ POST CODE ======================================
-
-function positive(i)
-{
-   if(i>=0)
-      return "+"+i;
-   return i;
-}
-
-function FlyLess() {
-try
-{
-   //var mouches = document.evaluate("//tr[@class='mh_tdpage']/td/img[@alt='Pas Là']/../../td[2]/text()[contains(.,'(')]",
-     //    document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-         
-    var mouches = document.evaluate("//img[@alt='Pas Là']/../../td[2]/text()[contains(.,'(')]",
-		document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-			          
-   listePouvoirs = new Array();
-   for(var i=0;i<mouches.snapshotLength;i++)
-   {
-      var texte = mouches.snapshotItem(i).nodeValue;
-      if(texte.indexOf(":")==-1)
-         continue;
-      var bonus = trim(texte.substring(texte.indexOf("(")+1,texte.indexOf(":")));
-      var valeur = parseInt(texte.substring(texte.indexOf(":")+1,texte.indexOf(")")));
-      if(listePouvoirs[bonus] == null)
-         listePouvoirs[bonus] = valeur;
-      else
-         listePouvoirs[bonus] += valeur;
-   }
-   var nombreMouches = document.evaluate("//tr[@class='mh_tdpage']/td[4]/text()[contains(.,'jours')]/../../td[2]/text()[contains(.,'(')]",
-         document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null).snapshotLength;
-   var tbody = document.evaluate("//form[@name='ActionForm']/table[@class='mh_tdborder']/tbody[1]",
-         document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-   tr=tbody.lastChild.lastChild;
-   
-   if (mouches.snapshotLength>0) {
-	   var td = appendTdText(tr,mouches.snapshotLength+" mouches absentes sur "+nombreMouches+"",1);
-	   td.setAttribute('colspan', 4);
-	   var text="";
- 	   for (key in listePouvoirs)
-	   {
- 	     if(text.length!=0)
- 	        text+=" | ";
-	      text+=key+" : "+positive(listePouvoirs[key]);
-	      if(key=="TOUR")
-	         text+=" min";
-	      if(key=="RM" || key=="MM")
-	         text+="%";
-	   }
-	   appendText(td," ( "+text+" )");
-	}
-}
-catch(e) {alert(e)}
-}
-
-FlyLess();
+//============================ ZZ POST CODE ======================================
+
+
+
+function positive(i)
+
+{
+
+   if(i>=0)
+
+      return "+"+i;
+
+   return i;
+
+}
+
+
+
+function FlyLess() {
+
+try
+
+{
+
+   //var mouches = document.evaluate("//tr[@class='mh_tdpage']/td/img[@alt='Pas Là']/../../td[2]/text()[contains(.,'(')]",
+
+     //    document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+
+         
+
+    var mouches = document.evaluate("//img[@alt='Pas Là']/../../td[2]/text()[contains(.,'(')]",
+
+		document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+
+			          
+
+   listePouvoirs = new Array();
+
+   for(var i=0;i<mouches.snapshotLength;i++)
+
+   {
+
+      var texte = mouches.snapshotItem(i).nodeValue;
+
+      if(texte.indexOf(":")==-1)
+
+         continue;
+
+      var bonus = trim(texte.substring(texte.indexOf("(")+1,texte.indexOf(":")));
+
+      var valeur = parseInt(texte.substring(texte.indexOf(":")+1,texte.indexOf(")")));
+
+      if(listePouvoirs[bonus] == null)
+
+         listePouvoirs[bonus] = valeur;
+
+      else
+
+         listePouvoirs[bonus] += valeur;
+
+   }
+
+   var nombreMouches = document.evaluate("//tr[@class='mh_tdpage']/td[4]/text()[contains(.,'jours')]/../../td[2]/text()[contains(.,'(')]",
+
+         document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null).snapshotLength;
+
+   var tbody = document.evaluate("//form[@name='ActionForm']/table[0]/tbody[1]",
+
+         document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+
+
+   
+
+   if (tbody && mouches.snapshotLength>0) {
+
+           tr=tbody.lastChild.lastChild;
+
+	   var td = appendTdText(tr,mouches.snapshotLength+" mouches absentes sur "+nombreMouches+"",1);
+
+	   td.setAttribute('colspan', 4);
+
+	   var text="";
+
+ 	   for (key in listePouvoirs)
+
+	   {
+
+ 	     if(text.length!=0)
+
+ 	        text+=" | ";
+
+	      text+=key+" : "+positive(listePouvoirs[key]);
+
+	      if(key=="TOUR")
+
+	         text+=" min";
+
+	      if(key=="RM" || key=="MM")
+
+	         text+="%";
+
+	   }
+
+	   appendText(td," ( "+text+" )");
+
+	}
+
+}
+
+catch(e) {alert(e)}
+
+}
+
+
+
+FlyLess();
+
